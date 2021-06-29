702e389b691962320a347272667ac7b236355a5f
RanNiv <nivran9@gmail.com>
2021-06-29 19:43:42 +0300

    add bootstrap

--- calculator-front/src/App.js
+++ calculator-front/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
 
 function App() {
+
+  const [value1,setValue1]=useState(null);
+  const [value2,setValue2]=useState(null);
+  const [result,setResult]=useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      Test
     </div>
   );
 }
