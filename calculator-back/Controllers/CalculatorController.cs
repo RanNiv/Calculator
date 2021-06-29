f60c4e3506f4cf2b716251bde443ec86122f4f90
RanNiv <nivran9@gmail.com>
2021-06-29 19:50:20 +0300

    add controller

---
+++ calculator-back/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace calculator_back.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class CalculatorController : ControllerBase
+    {
+        
+
+        
+
+     
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string [] {"Test1","Test2"};
+           
+        }
+    }
+}
